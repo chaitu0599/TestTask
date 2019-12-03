075a7395eb26bbc5bd6f2ee2e6ee9db0d772e184
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:24 +0530

    This is my test task demo project!

---
+++ MyProject/src/GetSet.java
@@ -0,0 +1,19 @@
+
+public class GetSet {
+	String MyBankName;
+	int MyBankAccNo;
+	public String getMyBankName() {
+		return MyBankName;
+	}
+	public void setMyBankName(String myBankName) {
+		MyBankName = myBankName;
+	}
+	public int getMyBankAccNo() {
+		return MyBankAccNo;
+	}
+	public void setMyBankAccNo(int myBankAccNo) {
+		MyBankAccNo = myBankAccNo;
+	}
+	
+
+}
